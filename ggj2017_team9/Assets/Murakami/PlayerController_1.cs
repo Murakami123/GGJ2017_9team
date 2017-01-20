172db40c53951aa64271d9293d41807c3fc601a8
Murakami <snowmanappli123@gmail.com>
2017-01-21 01:30:10 +0900

    とりあえず動く鮭

---
+++ ggj2017_team9/Assets/Murakami/PlayerController_1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_1 : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+
+    void Move()   {
+        Debug.Log("Moveメソッド");
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(moveX, moveY);
+
+
+    }
+
+
+}

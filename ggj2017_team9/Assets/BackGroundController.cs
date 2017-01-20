ec002e66f7160a281504fed81ab10a6b793749df
Murakami <snowmanappli123@gmail.com>
2017-01-21 01:11:56 +0900

    川が動く、とりあえず魚っぽい画像

---
+++ ggj2017_team9/Assets/BackGroundController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundController : MonoBehaviour{
+
+    public float zureY; 
+
+    void Update() {
+        transform.Translate(0, -0.1f, 0);
+        if (transform.position.y < -zureY) {
+            transform.position = new Vector3(0, zureY, 0);
+        }
+    }
+}
